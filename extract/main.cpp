e87c3a136d544032765b7a99863960fe1531fcbc
nucular <nucular@users.noreply.github.com>
2015-06-21 22:22:00 +0200

    Made extract a bit more verbose

--- extract/main.cpp
+++ extract/main.cpp
@@ -13,12 +13,27 @@ int main(int argc, char *argv[])
     fp = fopen(filename, "rb");
     if (!fp)
     {
+        printf("could not open %s\n", filename);
         return 1;
     }
 
-    if (extract_pxt(fp)) return 1;
-    if (extract_files(fp)) return 1;
-    if (extract_stages(fp)) return 1;
+    if (extract_pxt(fp)) {
+        printf("could not extract pxt\n");
+        fclose(fp);
+        return 1;
+    }
+    if (extract_files(fp)) {
+        printf("could not extract files\n");
+        fclose(fp);
+        return 1;
+    }
+    if (extract_stages(fp)) {
+        printf("could not extract stages\n");
+        fclose(fp);
+        return 1;
+    }
+
+    printf("done\n");
     fclose(fp);
     return 0;
 }
